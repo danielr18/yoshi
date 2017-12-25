2e90b380ee426377b2bcf2b2c6207b638e47d40e
Mantas Miliukas <mantasm@wix.com>
2017-12-25 23:11:15 +0200

    fix(env): to include CI env property
    and support Travis

--- yoshi/test/helpers/env-variables.js
+++ yoshi/test/helpers/env-variables.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  outsideTeamCity: {TEAMCITY_VERSION: '', BUILD_NUMBER: ''},
+  outsideTeamCity: {TEAMCITY_VERSION: '', BUILD_NUMBER: '', CI: ''},
   insideTeamCity: {TEAMCITY_VERSION: 1},
   insideWatchMode: {WIX_NODE_BUILD_WATCH_MODE: true},
   teamCityArtifactVersion: {ARTIFACT_VERSION: '1.0.0'},
